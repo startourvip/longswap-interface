ea82c179478f28b5f8d9d293cdf45d9041cb313e
startourvip <102037596+startourvip@users.noreply.github.com>
2025-09-15 17:17:53 +0800

    1

--- src/constants/tokens.js
+++ src/constants/tokens.js
@@ -3,6 +3,9 @@ import { Token } from "./types";
 export const WETH = {
   97: new Token(97, '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd', 18, 'WBNB', 'Wrapper BNB'),
   56: new Token(56, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapper BNB'),
+  196: [
+    new Token(196, '0x8B9c5C3eA7d1b6E8F2b3F4dBf4F1c3A9e5E5b5b5', 18, 'WOKB', 'Wrapped OKB'),
+  ]
 }
 
 export const TOKEN_LIST = {
@@ -14,6 +17,9 @@ export const TOKEN_LIST = {
     new Token(56, '0x55d398326f99059fF775485246999027B3197955', 18, 'USDT', 'Tether USD'),
     new Token(56, '0x3B4E1f58cd3A434E348bF335C6ae0C993d3789cB', 18, 'LONG-314', 'Long 314'),
     new Token(56, '0x003144B41d9743D402c5bdF3f72Ca0f327aA0Bca', 18, 'X314', 'X-314'),
+  ],
+  196: [
+    new Token(196, '0x8B9c5C3eA7d1b6E8F2b3F4dBf4F1c3A9e5E5b5b5', 18, 'WOKB', 'Wrapped OKB'),
   ]
 }
 
@@ -28,4 +34,7 @@ export const SUGGESTED_BASES = {
     new Token(56, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'BUSD', 'BUSD Token'),
     new Token(56, '0x2170Ed0880ac9A755fd29B2688956BD959F933F8', 18, 'ETH', 'Ethereum Token'),
   ],
+  196: [
+    new Token(196, '0x8B9c5C3eA7d1b6E8F2b3F4dBf4F1c3A9e5E5b5b5', 18, 'WOKB', 'Wrapped OKB'),
+  ]
 }
--- src/contexts/WalletProvider/index.js
+++ src/contexts/WalletProvider/index.js
@@ -4,18 +4,42 @@ import { bsc, bscTestnet } from 'wagmi/chains'
 import { WagmiProvider } from 'wagmi'
 import { injected } from 'wagmi/connectors'
 
+// 添加 OKX X Layer (id: 196)
+const xLayer = {
+  id: 196,
+  name: 'OKX X Layer',
+  network: 'okx-xlayer',
+  nativeCurrency: {
+    name: 'OKB',
+    symbol: 'OKB',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.xlayer.tech'],
+    },
+    public: {
+      http: ['https://rpc.xlayer.tech'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'OKX Explorer', url: 'https://www.oklink.com/xlayer' },
+  },
+}
+
 const queryClient = new QueryClient()
 
 bsc.rpcUrls.default.http = ['https://bsc-dataseed1.bnbchain.org', 'https://bsc-dataseed2.defibit.io']
 
 export const config = createConfig({
-  chains: [bsc],
+  chains: [bsc, xLayer], // 添加 xLayer
   connectors: [
     injected()
   ],
   transports: {
     [bsc.id]: http(),
     [bscTestnet.id]: http(),
+    [xLayer.id]: http(),
   },
 })
 
